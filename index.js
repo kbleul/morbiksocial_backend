8db3bc875c5438db5cf3afccd0de85628d780209
kiBROm <danu@localhost.localdomain>
2022-10-06 07:55:55 -0400

    ready to deploy 2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
 const helmet = require("helmet")
+const connectDB = require('./config/dbConn')
 
 const userAuthRoutes = require("./routes/userAuthRoutes")
 const userRoutes = require("./routes/userRoutes")
@@ -40,13 +41,20 @@ app.use("/public", express.static(path.join(__dirname, 'public')));
         console.log(req.path, req.method , req.body)     
         next()
     })
+  
+  connectDB()
 
 
-  mongoose.connect(process.env.MONGO_URI)
-            .then(() => {
-                app.listen(process.env.PORT , () => { console.log("Server is running at " + process.env.PORT )})
-             })
-              .catch(error => console.log(error))
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB')
+  app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
+})
+
+  // mongoose.connect(process.env.MONGO_URI)
+  //           .then(() => {
+  //               app.listen(process.env.PORT , () => { console.log("Server is running at " + process.env.PORT )})
+  //            })
+  //             .catch(error => console.log(error))
     
     app.use("/api/auth", userAuthRoutes)
     app.use("/api/user/profile", upload.single('avatar'), userRoutes )
