967d610fae4a33204b59f234bf696ec9354056d5
kbleul <kbleulseged@gmail.com>
2022-10-07 14:38:55 -0400

    remove nodemon deploy trial 14

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const app = express()
 app.use("/public", express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
     //middlewares
     //app.use(helmet())
 
